a4535cc7b019e28fb2cc281b2a87c5b6b6af37bc
Anas <anas.mamlouk@yahoo.fr>
2024-03-07 19:44:08 +0100

    Day 7 with playwright

---
+++ untitled/tests/day7-multiple_dropdowns.spec.js
@@ -0,0 +1,25 @@
+/*
+URL : https://testautomationpractice.blogspot.com/
+npx playwright test tests/day7-multiple_dropdowns.spec.js --headed --project chromium
+
+*/
+import{test,expect} from "@playwright/test";
+test("Multiple drop down tests", async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com");
+
+    // select multiple options from multi select dropdown
+    await page.selectOption("//*[@id='colors']",['Blue','Red'])
+
+    //Assertion
+    const options = await page.locator("//*[@id='colors']/option")
+    await expect(options).toHaveCount(5)
+
+    const options2 = await page.$$("//*[@id='colors']/option")
+    await expect(options2).toHaveLength(5)
+    console.log("Number of options :",options2.length)
+
+    const content = await page.locator("#colors").textContent()
+    await expect(content.includes('Blue')).toBeTruthy()
+
+    await page.waitForTimeout(5000)
+})

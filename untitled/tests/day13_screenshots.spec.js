7f777d9430d507857617756f5ebbfb18f5512461
Anas <anas.mamlouk@yahoo.fr>
2024-03-16 16:21:49 +0100

    Day 13 with playwright

--- untitled/tests/day13_screenshots.spec.js
+++ untitled/tests/day13_screenshots.spec.js
@@ -4,13 +4,19 @@ npx playwright test tests/day13_screenshots.spec.js --headed --project chromium
 */
 
 import{test,expect} from "@playwright/test";
-test.only("Test page screenshot", async ({page})=> {
+test("Test page screenshot", async ({page})=> {
     await page.goto("https://demo.opencart.com/")
     await page.screenshot({path:"tests/Screenshots/"+Date.now()+"Homepage.png"})
 })
 test("Full page screenshot", async ({page})=> {
-
+    await page.goto("https://demo.opencart.com/")
+    await page.waitForTimeout(5000)
+    await page.screenshot({path:"tests/Screenshots/"+Date.now()+"FullHomepage.png",fullPage:true})
+    await page.waitForTimeout(1000)
 })
-test("Element page screenshot", async ({page})=> {
-
+test.only("Element page screenshot", async ({page})=> {
+    await page.goto("https://demo.opencart.com/")
+    await page.waitForTimeout(5000)
+    const Macbook_div = await page.locator("//*[@id='content']/div[2]/div[1]/form/div")
+    await Macbook_div.screenshot({path:"tests/Screenshots/"+Date.now()+"Macbook.png"})
 })
